55db17e613e19833ea593a5f29771894b4cdb959
ysk-94 <00ysk.tech@gmail.com>
2020-02-12 07:39:32 +0900

    完了

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import TodoApp from './components/TodoApp';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import tasksReducer from './reducers/tasks';
 
 const store = createStore(tasksReducer);
 
 
-function renderApp(store) {
-  render (
-    <TodoApp store={store} />,
-    document.getElementById('root')
-  );
-}
+render (
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+);
 
-store.subscribe(() => renderApp(store));
-renderApp(store);
